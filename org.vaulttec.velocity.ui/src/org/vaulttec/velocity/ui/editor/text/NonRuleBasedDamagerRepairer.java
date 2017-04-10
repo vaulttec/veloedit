c73e5791f60eaccab114c8ebd1cdcaf1fe99b4ff
Torsten Juergeleit <torsten.juergeleit@gmail.com>
2017-04-10 23:50:41 +0200

    adds copyright header

--- org.vaulttec.velocity.ui/src/org/vaulttec/velocity/ui/editor/text/NonRuleBasedDamagerRepairer.java
+++ org.vaulttec.velocity.ui/src/org/vaulttec/velocity/ui/editor/text/NonRuleBasedDamagerRepairer.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2003 Torsten Juergeleit.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *     Torsten Juergeleit - initial API and implementation
+ *******************************************************************************/
 package org.vaulttec.velocity.ui.editor.text;
 
 import org.eclipse.core.runtime.Assert;
